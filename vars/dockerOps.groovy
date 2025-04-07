765a93a41583f56be159955073ac36e736de50a5
nani454554 <119169444+nani454554@users.noreply.github.com>
2025-04-07 15:37:23 +0530

    Update dockerOps.groovy

--- vars/dockerOps.groovy
+++ vars/dockerOps.groovy
@@ -1,22 +1,14 @@
 def call(String imageName) {
-    pipeline {
-        agent any
-        stages {
-            stage('Docker Build') {
-                steps {
-                    sh "docker build -t ${imageName} ."
-                }
-            }
-            stage('Docker Push') {
-                steps {
-                    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-                        sh """
-                            echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-                            docker push ${imageName}
-                        """
-                    }
-                }
-            }
+    stage('Docker Build') {
+        sh "docker build -t ${imageName} ."
+    }
+
+    stage('Docker Push') {
+        withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+            sh """
+                echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+                docker push ${imageName}
+            """
         }
     }
 }
