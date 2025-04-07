d63e8ea0c96b3c6341bde682106686970f3d2613
nani454554 <119169444+nani454554@users.noreply.github.com>
2025-04-07 15:40:52 +0530

    Update buildJavaApp.groovy

--- vars/buildJavaApp.groovy
+++ vars/buildJavaApp.groovy
@@ -1,6 +1,15 @@
 def call() {
     stage('Checkout') {
-        checkout scm
+        checkout([
+          $class: 'GitSCM',
+          branches: [[name: '*/main']],
+          userRemoteConfigs: [[url: 'https://github.com/your-org/your-java-app.git']]
+        ])
+    }
+
+    stage('Debug Workspace') {
+        sh 'pwd'
+        sh 'ls -la'
     }
 
     stage('Build with Maven') {
