d9d3913263dc7836cf1c80d5035b4dc37d63ebe5
nani454554 <119169444+nani454554@users.noreply.github.com>
2025-04-07 17:54:39 +0530

    Update dockerOps.groovy

--- vars/dockerOps.groovy
+++ vars/dockerOps.groovy
@@ -1,15 +1,24 @@
-def call(String imageName) {
-    stage('Docker Build') {
-         withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-            sh """
-                echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-        sh "docker build -t ${imageName} ."
-    }
+// def call(String imageName) {
+//     stage('Docker Build') {
+//          withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+//             sh """
+//                 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+//         sh "docker build -t ${imageName} ."
+//     }
 
-    stage('Docker Push') {
+//     stage('Docker Push') {
        
-                docker push ${imageName}
-            """
-        }
+//                 docker push ${imageName}
+//             """
+//         }
+//     }
+// }
+def call(String imageName) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+        sh """
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            docker build -t ${imageName} .
+            docker push ${imageName}
+        """
     }
 }
